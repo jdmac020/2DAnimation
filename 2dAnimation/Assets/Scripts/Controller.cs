d0242c551ccc0c63d2c68d23714cabdc21acea20
jdmac020 <john.macgregor@gmail.com>
2020-01-04 12:50:14 -0500

    Add first failing test

--- 2dAnimation/Assets/Scripts/Controller.cs
+++ 2dAnimation/Assets/Scripts/Controller.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public Rigidbody RigidBody { get; set; }
+
     // Start is called before the first frame update
     void Start()
     {
--- 2dAnimation/Assets/Scripts/Tests/ControllerTest.cs
+++ 2dAnimation/Assets/Scripts/Tests/ControllerTest.cs
@@ -6,19 +6,22 @@ using UnityEngine.TestTools;
 
 namespace Tests
 {
-    public class ControllerTests
+    public class ControllerTest
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void ControllerTestsSimplePasses()
+        public void ControllerTestSimplePasses()
         {
+            var controller = new GameObject().AddComponent<Controller>();
             // Use the Assert class to test conditions
+            Assert.IsInstanceOf<Rigidbody2D>(controller.RigidBody);
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator ControllerTestsWithEnumeratorPasses()
+        [Ignore("This ain't a real thing yet")]
+        public IEnumerator ControllerTestWithEnumeratorPasses()
         {
             // Use the Assert class to test conditions.
             // Use yield to skip a frame.
